40cf6913a2990c38843dc3dce5fcbe8d8ef61967
yiiow1309 <55523982+yiiow1309@users.noreply.github.com>
2020-09-24 17:05:18 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, num1;
+            //string ipt;
+            Console.WriteLine("Hello C# world");
+            Console.Write("첫번째 정수 입력:");
+            /*
+            ipt = Console.ReadLine();
+            num = int.Parse(ipt);
+            */
+            num = int.Parse(Console.ReadLine());
+            Console.Write("두번째 정수 입력:");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(num + "+" + num1 + "=" + (num+ num1));
+            Console.WriteLine(num + "-" + num1 + "=" + (num - num1));
+            Console.WriteLine(num + "*" + num1 + "=" + (num * num1));
+            Console.WriteLine(num + "/" + num1 + "=" + (num / num1));
+
+        }
+    }
+}
